e9e757a74d1134675b8d66d7b8d3a8b58183359c
Jithin K Satheesh <jithinksatheesh@gmail.com>
2025-01-10 21:27:26 +0530

    init

--- Build_for_git_host/_next/static/chunks/pages/home-9fa7d76f3657f01e.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[229],{8862:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/home",function(){return u(352)}])}},function(n){n.O(0,[247,98,658,576,352,888,774,179],function(){return n(n.s=8862)}),_N_E=n.O()}]);
